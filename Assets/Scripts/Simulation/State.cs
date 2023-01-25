98006223d67d81dedfc8646a9e881a39c3460c04
karstro <33285644+karstro@users.noreply.github.com>
2023-01-25 16:01:17 +0100

    refactor and comment the visualizer and components

--- Assets/Scripts/Simulation/State.cs
+++ Assets/Scripts/Simulation/State.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// class that contains the state of a simulation step at a certain time
 public readonly struct State
 {
     public readonly float time;
@@ -18,6 +19,20 @@ public readonly struct State
         this.time = time;
         this.Position = c.Position;
         this.Rotation = c.Rotation;
+    }
+
+    public static State Lerp(State A, State B, float fraction) {
+        float InterpolatedTime = Mathf.Lerp(A.time, B.time, fraction);
+        Vector3 InterpolatedPosition = Vector3.Lerp(A.Position, B.Position, fraction);
+        Quaternion InterpolatedRotation = Quaternion.Slerp(A.Rotation, B.Rotation, fraction);
+        return new State(InterpolatedTime, InterpolatedPosition, InterpolatedRotation);
+    }
+
+    public static State LerpByTime(State Before, State After, float t) {
+        float fraction = Mathf.InverseLerp(Before.time, After.time, t);
+        Vector3 InterpolatedPosition = Vector3.Lerp(Before.Position, After.Position, fraction);
+        Quaternion InterpolatedRotation = Quaternion.Slerp(Before.Rotation, After.Rotation, fraction);
+        return new State(t, InterpolatedPosition, InterpolatedRotation);
     }
 
     public override string ToString() {
---
+++ Assets/Scripts/visualization/CreatureModelBuilder.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreatureModelBuilder
+{
+    // build a sphere at the given position
+    private static void BuildSphereAt(Transform parent, Vector3 Pos, float Diameter, string Name) {
+        GameObject Sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        Sphere.transform.parent = parent;
+        Sphere.transform.localPosition = Pos;
+        Sphere.transform.localScale = Diameter * Vector3.one;
+        Sphere.name = Name;
+    }
+
+    // build a cylinder whose ends are at the given start and end positions
+    private static void BuildCylinderFromTo(Transform parent, Vector3 Start, Vector3 End, float Width, string Name) {
+        GameObject Cylinder = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+        Cylinder.transform.parent = parent;
+        Cylinder.transform.localPosition = (Start + End) / 2;
+        Vector3 StartToEnd = End - Start;
+        Cylinder.transform.localRotation = Quaternion.FromToRotation(Vector3.up, StartToEnd);
+        Cylinder.transform.localScale = new Vector3(Width, StartToEnd.magnitude / 2, Width);
+        Cylinder.name = Name;
+    }
+
+    // build the model of the individual jet indicated by the jetIndex
+    private static void BuildJet(Transform parent, Creature c, int JetIndex) {
+        // Get the positions of the jet's start and end
+        (Vector3 JetStartPos, Vector3 JetEndPos) = c.GetJetStartAndEnd(JetIndex);
+        // turn them into the positions relative to the creature's origin
+        JetStartPos -= c.Position;
+        JetEndPos -= c.Position;
+        float JetWidth = 2 * c.JetRadius;
+        string JetName = "Jet" + JetIndex.ToString();
+        // Build the spheres at the jet's start and end
+        CreatureModelBuilder.BuildSphereAt(parent, JetStartPos, JetWidth, JetName + "Start");
+        CreatureModelBuilder.BuildSphereAt(parent, JetEndPos, JetWidth, JetName + "End");
+        // Build Cylinders from Center to jet's start, then to jet's end
+        CreatureModelBuilder.BuildCylinderFromTo(parent, Vector3.zero, JetStartPos, JetWidth, JetName + "Arm");
+        CreatureModelBuilder.BuildCylinderFromTo(parent, JetStartPos, JetEndPos, JetWidth, JetName);
+    }
+
+    // build the central body of the creature
+    private static void BuildBody(Transform parent, Creature c) {
+        // #TODO replace body sphere with utah teapot. thanks i hate it.
+        // body is currently just a sphere
+        CreatureModelBuilder.BuildSphereAt(parent, Vector3.zero, 2 * c.Radius, "Body");
+    }
+
+    // Build the GameObjects that will be used to visualize the creature's simulation in the scene
+    public static GameObject BuildCreatureModel(Creature c, string name) {
+        // create the parent object
+        GameObject Object = new GameObject();
+
+        // set the parent object's properties
+        Object.transform.SetPositionAndRotation(c.Position, c.Rotation);
+        Object.name = name;
+
+        // build each jet and the body
+        for (int i = 0; i < c.Jets; i++) {
+            CreatureModelBuilder.BuildJet(Object.transform, c, i);
+        }
+        CreatureModelBuilder.BuildBody(Object.transform, c);
+
+        // Return the parent object that now contains all the elements of the creature model
+        return Object;
+    }
+}
